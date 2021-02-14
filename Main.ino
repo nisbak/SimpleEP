3a5dc26e1c60359a2c841e590a5f4345e11687f9
nisbak <nisbak@gmail.com>
2021-02-14 20:11:16 +0100

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -2,21 +2,33 @@
 
 //#define USE_SERIAL  SerialUSB //Serial for boards whith USB serial port
 #define USE_SERIAL  Serial
-#define outputPin  12 
+#define outputPin  12
+#define minPWM  10 
+#define GroupSolenoid  5 
 
 //dimmerLamp dimmer(outputPin, zerocross); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
 dimmerLamp dimmer(outputPin); //initialase port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
-
+//Zero-cross on D2 for MEGA
 int outVal = 0;
 
 void setup() {
   USE_SERIAL.begin(9600);
-  dimmer.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE) 
+  dimmer.begin(NORMAL_MODE, OFF); //dimmer initialisation: name.begin(MODE, STATE) 
+  pinMode(GroupSolenoid, OUTPUT);
+  digitalWrite(GroupSolenoid, LOW);  //
 }
 
 void loop() 
 {
   outVal = map(analogRead(0), 1, 1024, 100, 0); // analogRead(analog_pin), min_analog, max_analog, 100%, 0%);
   USE_SERIAL.println(outVal); 
-  dimmer.setPower(outVal); // name.setPower(0%-100%)
+  if (outVal > minPWM) {
+    dimmer.setPower(outVal);  
+    dimmer.setState(ON);
+    digitalWrite(GroupSolenoid, HIGH);
+  }
+  else {
+    dimmer.setState(OFF);
+    digitalWrite(GroupSolenoid, LOW);
+  }
 }
